71492513ef278c46b5a689226c74ce501bdbb85e
Sébastien Chopin <seb@nuxt.com>
2023-12-13 14:21:32 +0100

    chore: code-split deno deps

--- server/utils/kv.ts
+++ server/utils/kv.ts
@@ -1,8 +1,9 @@
-import { openKv } from '@deno/kv'
+const OpenKV = () => import('@deno/kv')
 
-export const useKv = () => {
+export const useKv = async () => {
   if (globalThis.Deno) {
     return globalThis.Deno.openKv()
   }
+  const { openKv } = await OpenKV()
   return openKv('kv.db')
 }
